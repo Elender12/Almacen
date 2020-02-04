fc2831f6b188a80edbee4ae9056a081c84b0f6fa
Elena <elender1230@gmail.com>
2020-02-04 16:43:16 +0100

    "comments"

--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Usuario::class, function (Faker $faker) {
     return [
+        //se generan datos aleatorios
         'nombre'=>$faker->sentence(1),
         'email'=>$faker->sentence(1),
         'rol'=>$faker->sentence(1),
