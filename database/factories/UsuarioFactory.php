a467b48f60aaf6c25b328796538ac83a19710a1e
elena <elender1230@gmail.com>
2020-02-12 19:08:12 +0100

    subidda

--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -8,8 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Usuario::class, function (Faker $faker) {
     return [
         //se generan datos aleatorios
-        'nombre'=>$faker->sentence(1),
-        'email'=>$faker->sentence(1),
-        'rol'=>$faker->sentence(1),
+        'nombre'=>$faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'rol'=>$faker->company,
         ];
 });
